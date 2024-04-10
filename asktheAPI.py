af30c2e86d7c96d788b602acfed977f1d88392de
wdpetry <william.petry@ensign.edu>
2024-04-10 12:15:43 -0600

    Update asktheAPI.py

--- asktheAPI.py
+++ asktheAPI.py
@@ -1,3 +1,13 @@
 import requests
 
 
+url = "https://api.openweathermap.org/data/2.5/weather?lat=39.6753592&lon=-104.783926&appid=88b2b960069a116def666a02c973d216"
+response = requests.get(url)
+if response.status_code == 200:
+    d = response.json()
+    print(d)
+    print(f"The temperature is {d['main']['temp']} Kelvin")
+else:
+    print(f"Failed to retrieve data. Status code: {response.status_code}")
+
+
